309928a2bbcb2223514cb2854491af9101f09b43
Nathan de Witt <dewittnathan@gmail.com>
2020-08-23 09:52:43 +0200

    Added levels

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 25f;
+    [SerializeField] AudioClip mainEngine;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+
+    State state;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,45 +29,85 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void Rotate()
+    void OnCollisionEnter(Collision collision)
     {
-        float rcsThrust = 100f; 
+        if (state != State.Alive)
+            return;
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // Do nothing
+                break;
+
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 2f);
+                break;
+
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 2f);
+                break;
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToRotateInput()
+    {
+        
+        float roatationThisFrame = rcsThrust * Time.deltaTime;
 
         rigidBody.freezeRotation = true;
 
         if (Input.GetKey(KeyCode.D))
         {
-            float roatationThisFrame = rcsThrust * Time.deltaTime;
             transform.Rotate(-Vector3.forward * roatationThisFrame);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            float rotateSpeed = rcsThrust * Time.deltaTime;
-            transform.Rotate(Vector3.forward * rotateSpeed);
+            transform.Rotate(Vector3.forward * roatationThisFrame);
         }
 
         rigidBody.freezeRotation = false;
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else if (audioSource.isPlaying)
         {
             audioSource.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
         }
     }
 }
